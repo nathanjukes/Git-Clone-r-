4494567123aabe7f83d26b55f96de2f4811a312c
nathanjukes <njukes1@gmail.com>
2020-07-23 15:30:47 +0100

    Added options for which repo(s) to clone

--- Git-Clone(r)/Git-Clone(r)/API Classes/GetRepos.cs
+++ Git-Clone(r)/Git-Clone(r)/API Classes/GetRepos.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 
 namespace Git_Clone_r_.API_Classes
 {
@@ -17,27 +19,21 @@ namespace Git_Clone_r_.API_Classes
 
             if(type == "public")
             {
-                GetPublicRepos();
+                _gitLinks = GetPublicRepos();
+                PromptForUserChoice();
             }
             else if(type == "private")
             {
                 //Get private repos
             }
-           
-
-            foreach(var i in _gitLinks)
-            {
-                //Console.WriteLine(i.Key + $" : ({i.Value})");
-                Clone(i.Value);
-            }
         }
 
-        public static void GetPublicRepos() //From a user
+        public static Dictionary<string, string> GetPublicRepos() //From a user
         {
             //Prompt for username to clone from
             Console.WriteLine("Please input the user you wish to clone from: ");
             string username = Console.ReadLine();
-            _gitLinks = GetRepoLinks(username);
+            return GetRepoLinks(username);
         }
 
         private static Dictionary<string, string> GetRepoLinks(string username)
@@ -58,9 +54,68 @@ namespace Git_Clone_r_.API_Classes
             return _gitLinks;
         }
 
+        private static void PromptForUserChoice()
+        {
+            Console.WriteLine("\n\n═════╣ Available Repositories ╠═════\n");
+
+            for (int i = 0; i < _gitLinks.Count; i++)
+            {
+                Console.WriteLine($"{i+1}) {_gitLinks.ElementAt(i).Key}");
+            }
+
+            Console.WriteLine("\n[ '1' - Clones the first repo || '1-5' - Clones the repos within that range (inclusively) || 'X' - Clones every repo ]\n\nPlease input beneath:");
+
+            string userInput = Console.ReadLine().Trim();
+
+            if(userInput.ToUpper() == "X")
+            {
+                Console.WriteLine("\nCloning every repository\n");
+
+                foreach(var i in _gitLinks.Values)
+                {
+                    Clone(i);
+                }
+            }
+            else if(userInput.Contains("-"))
+            {
+                try
+                {
+                    int lowIndex = Convert.ToInt32(userInput.Substring(0, userInput.IndexOf('-')));
+                    int highIndex = Convert.ToInt32(userInput.Substring(userInput.IndexOf('-') + 1, userInput.Length - userInput.IndexOf('-') - 1));
+
+                    if(highIndex > _gitLinks.Count || lowIndex <= 0)
+                    {
+                        Console.WriteLine("Incorrect Bounds");
+                        return;
+                    }
+
+                    for(int i = lowIndex; i <= highIndex; i++)
+                    {
+                        Clone(_gitLinks.ElementAt(i).Value);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+            else
+            {
+                try
+                {
+                    Clone(_gitLinks.ElementAt(Convert.ToInt32(userInput)).Value);
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Incorrect input");
+                }
+            }
+        }
+
         private static void Clone(string cloneLink)
         {
-            Process process = Process.Start("CMD.EXE", $@"/C {_userSettings["defaultDirectory"].Substring(0, 1)}:&&cd {_userSettings["defaultDirectory"]}&&git clone https://github.com/nathanjukes/nathanjukes.git&&echo Repo Cloned to {_userSettings["defaultDirectory"]}");
+            Console.WriteLine($"\n{cloneLink}");
+            Process process = Process.Start("CMD.EXE", $@"/C {_userSettings["defaultDirectory"].Substring(0, 1)}:&&cd {_userSettings["defaultDirectory"]}&&git clone https://github.com/nathanjukes/nathanjukes.git {_gitLinks.Where(x => x.Value == cloneLink).FirstOrDefault().Key}&&echo Repo Cloned to {_userSettings["defaultDirectory"]}");
             process.WaitForExit();
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Git-Clone-r-
-A C# script for cloning your repositories efficiently. Primarily used to backup and clone all of your repositories on a new PC but has more capabilities.
+A C# CLI driven program for cloning your repositories efficiently. Primarily used to backup and clone all of your repositories on a new PC but has a few more features.
+
+
